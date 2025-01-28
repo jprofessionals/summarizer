11d75f027a0d2c0eb43000668d17900ef3428514
tobiasbj <7911343+tobiasbj@users.noreply.github.com>
2025-01-28 20:35:34 +0100

    added terraform example

---
+++ terraform/main_example.tf
@@ -0,0 +1,44 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "myAppServicePlan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "myAppService"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    linux_fx_version = "DOCKER|myContainerRegistry.azurecr.io/my-fastapi-app:latest"
+  }
+
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+    DOCKER_REGISTRY_SERVER_URL          = "https://myContainerRegistry.azurecr.io"
+    DOCKER_REGISTRY_SERVER_USERNAME     = "myContainerRegistry"
+    DOCKER_REGISTRY_SERVER_PASSWORD     = "your_acr_password"
+  }
+}
+
+resource "azurerm_container_registry" "example" {
+  name                = "myContainerRegistry"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
