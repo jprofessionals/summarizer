cfa752c8ff250213b050797be009f6bee8b571f4
tobiasbj <7911343+tobiasbj@users.noreply.github.com>
2025-01-31 19:40:10 +0100

    adding container and openapi-key

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,7 @@ data "azurerm_client_config" "current" {}
 
 locals {
   current_user_id = coalesce(var.msi_id, data.azurerm_client_config.current.object_id)
+  envs = { for tuple in regexall("(.*)=(.*)", file("../.env")) : tuple[0] => sensitive(tuple[1]) }
 }
 
 resource "azurerm_container_registry" "summarizer" {
@@ -21,6 +22,43 @@ resource "azurerm_container_registry" "summarizer" {
   location            = azurerm_resource_group.summarizer.location
   sku                 = "Basic"
   admin_enabled       = true
+}
+
+resource "azurerm_container_group" "summarizer" {
+  name                = "jpro-summarizer-container-group"
+  location            = azurerm_resource_group.summarizer.location
+  resource_group_name = azurerm_resource_group.summarizer.name
+  os_type             = "Linux"
+
+  container {
+    name   = "summarizer"
+    image  = "${azurerm_container_registry.summarizer.login_server}/summarizer-app:latest"
+    cpu    = "1.0"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+
+    environment_variables = {
+      "ENV_VAR_NAME" = "value"
+    }
+
+    secure_environment_variables = {
+      "OPENAI_API_KEY" = azurerm_key_vault_secret.openai_api_key.value
+    }
+  }
+
+  image_registry_credential {
+    server   = azurerm_container_registry.summarizer.login_server
+    username = azurerm_container_registry.summarizer.admin_username
+    password = azurerm_container_registry.summarizer.admin_password
+  }
+
+  tags = {
+    environment = "testing"
+  }
 }
 
 resource "azurerm_key_vault" "summarizer" {
@@ -37,6 +75,11 @@ resource "azurerm_key_vault" "summarizer" {
   }
 }
 
+resource "azurerm_key_vault_secret" "openai_api_key" {
+  name         = "OpenAI-API-Key"
+  value        = local.envs["OPENAI_API_KEY"]
+  key_vault_id = azurerm_key_vault.summarizer.id
+}
 
 //  terraform init --upgrade
 //  terraform plan -out=tfplan
