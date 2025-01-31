9f7b64f05706fbef93fa90aa4fea84e098cbc68c
tobiasbj <7911343+tobiasbj@users.noreply.github.com>
2025-01-31 16:45:24 +0100

    Initial working terraform setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,25 @@
 // filepath: summarizer/terraform/main.tf
 provider "azurerm" {
+  subscription_id = var.subscription_id
   features {}
 }
 
 resource "azurerm_resource_group" "summarizer" {
-  name     = "example-resources"
+  name     = "summarize-resources"
   location = "West Europe"
 }
 
 resource "azurerm_container_registry" "summarizer" {
-  name                = "summarizeracr"
+  name                = "jprosummarizeracr"
   resource_group_name = azurerm_resource_group.summarizer.name
   location            = azurerm_resource_group.summarizer.location
   sku                 = "Basic"
   admin_enabled       = true
 }
 
-output "acr_login_server" {
-  value = azurerm_container_registry.summarizer.login_server
-}
 
-output "acr_admin_username" {
-  value = azurerm_container_registry.summarizer.admin_username
-}
 
-output "acr_admin_password" {
-  value = azurerm_container_registry.summarizer.admin_password
-}
+//  terraform init --upgrade
+//  terraform plan -out=tfplan
+//  terraform apply tfplan
+//  terraform destroy
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "subscription_id" {
+  description = "The subscription ID for the Azure account."
+  type        = string
+}
+
+variable "client_id" {
+  description = "The client ID for the Azure Service Principal."
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The client secret for the Azure Service Principal."
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "The tenant ID for the Azure account."
+  type        = string
+}
